b6c7904dd0d729d12283fa23da0532d20a85676c
lalabib <labibla21@gmail.com>
2024-03-26 17:39:45 +0700

    initiate commit

---
+++ app/src/main/java/com/lalabib/latihan/simpleproduct/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.lalabib.latihan.simpleproduct.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lalabib.latihan.simpleproduct.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
